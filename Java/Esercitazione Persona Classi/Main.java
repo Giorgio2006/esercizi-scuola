b65035978da8ae28a3ea6761d944e513473b44b4
Giorgio2006 <92974955+Giorgio2006@users.noreply.github.com>
2023-04-18 08:41:57 +0200

    Update Main.java

--- Java/Esercitazione Persona Classi/Main.java
+++ Java/Esercitazione Persona Classi/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
-    static Persona p1 = new Persona("Mario", "Rossi", "M", "Bisex", "Roma", "Biondo", 20, 1.80f, 80.0f);
-    static Persona p2 = new Persona("Giulia", "Bianchi", "M", "Lesbica", "Milano", "Castano", 19, 1.70f, 60.0f);
+    static Persona p1 = new Persona("Carlo", "De Rossi", "M", "Etero", "Roma", "Biondo", 20, 1.80f, 80.0f);
+    static Persona p2 = new Persona("Ornella", "Bianchi", "F", "Etero", "Milano", "Castano", 19, 1.70f, 60.0f);
     public static void main(String[] args) {
         p1.showData();
         System.out.println("***********************************************");
